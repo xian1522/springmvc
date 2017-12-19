727194c20522a3af67db3fff0570e38bfdb23c70
xian1522 <xian1522@126.com>
2017-12-19 13:54:51 +0800

    test

--- src/main/java/springmvc/web/HomeControllerTest.java
+++ src/main/java/springmvc/web/HomeControllerTest.java
@@ -5,15 +5,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.util.UriUtils;
 
 
 public class HomeControllerTest {
 	@Test
 	public void testHomePage() throws Exception{
-		UriUtils.decode("going going", "utf-8");
-//		HomeController controller = new HomeController();
-//		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-//		mockMvc.perform(MockMvcRequestBuilders.get("/homepage")).andExpect(MockMvcResultMatchers.view().name("index"));
+		HomeController controller = new HomeController();
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		mockMvc.perform(MockMvcRequestBuilders.get("/homepage")).andExpect(MockMvcResultMatchers.view().name("index"));
 	}
 }
