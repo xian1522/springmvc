6a6423d35fab1ef482cf8efd9b05a24281f8c422
xian1522 <xian1522@126.com>
2017-12-21 10:50:27 +0800

    过滤器设置

--- src/main/java/springmvc/config/WebAppInitializer.java
+++ src/main/java/springmvc/config/WebAppInitializer.java
@@ -1,18 +1,27 @@
 package springmvc.config;
 
+import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletRegistration.Dynamic;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebAppInitializer extends
 		AbstractAnnotationConfigDispatcherServletInitializer {
-
+	
+	/**
+	 * 指定spring.ioc配置类
+	 */
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		return new Class<?>[] {RootConfig.class};
 	}
-
+	
+	/**
+	 * 指定spring.mvc配置类
+	 */
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		return new Class<?>[] {WebConfig.class};
@@ -29,5 +38,18 @@ public class WebAppInitializer extends
 	@Override
 	protected void customizeRegistration(Dynamic registration){
 		registration.setMultipartConfig(new MultipartConfigElement("/temp/uploads"));
+	}
+	
+	/**
+	 * 设置过滤器
+	 */
+	protected Filter[] getServletFilters(){
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+		encodingFilter.setEncoding("utf-8");
+		encodingFilter.setForceEncoding(true);
+		return new Filter[] {
+			encodingFilter,
+			new HiddenHttpMethodFilter()
+		};
 	}
 }
