abc09b62b6943deb3fe8331048f6eaa0c89f9d27
xian1522 <xian1522@126.com>
2017-12-21 17:01:14 +0800

    集成hibernate

--- src/main/java/springmvc/config/RootConfig.java
+++ src/main/java/springmvc/config/RootConfig.java
@@ -1,13 +1,55 @@
 package springmvc.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages={"springmvc"},excludeFilters={@Filter(type=FilterType.ANNOTATION,value=EnableWebMvc.class)})
 public class RootConfig {
+	@Bean
+    public DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/springmvc?useUnicode=true&characterEncoding=utf8");
+        dataSource.setUsername("wj");
+        dataSource.setPassword("wj");
+        dataSource.setMinIdle(10);
+        dataSource.setMaxActive(100);
+        dataSource.setMaxWait(5000);
+        dataSource.setMinEvictableIdleTimeMillis(30000);
+        dataSource.setPoolPreparedStatements(true);
+        return dataSource;
+    }
+	
+	@Bean
+    public LocalSessionFactoryBean localSessionFactoryBean(DataSource dataSource) {
+		LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setPackagesToScan("springmvc.data");
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        hibernateProperties.setProperty("hibernate.format_sql", "true");
+        factoryBean.setHibernateProperties(hibernateProperties);
+        return factoryBean;
+    }
+	
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory);
+        return transactionManager;
+    }
 
 }
